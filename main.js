c33eb4e2252b3dfa4490200ff26c939ed2dc9231
niknek <62116442+niknek@users.noreply.github.com>
2020-09-28 12:10:54 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿window.addEventListener("load", () => {
+    const canvas = document.querySelector('#canvas')
+    const context = canvas.getContext('2d')
+
+    canvas.height = 600
+    canvas.width = 800
+
+
+    let penDown = false;
+
+    function draw(e) {
+        if (!penDown) return
+        context.lineTo(e.clientX, e.clientY)
+        context.stroke()
+
+    }
+
+
+    canvas.addEventListener('mousedown', () => { penDown = true; })
+    canvas.addEventListener('mouseup', () => { context.beginPath(); penDown = false; })
+    canvas.addEventListener('mousemove', draw)
+
+})
+
